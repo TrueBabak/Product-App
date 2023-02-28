2c354d43a0320fa23c38dcefb657c625aff0d578
Babak <jahanibabak80@gmail.com>
2023-03-01 01:06:53 +0330

    click handler in product list

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Products from "../Components/Products";
 
 const App = () => {
-  const productslist = [
+  const [Product, setProduct] = useState([
     { name: "react", price: "100$" },
     { name: "vue", price: "200$" },
     { name: "angular", price: "300$" },
-  ];
+  ]);
+  const productHandler = () => {
+    setProduct([
+      { name: "react", price: "15$" },
+      { name: "vue", price: "27$" },
+      { name: "angular", price: "39$" },
+    ]);
+  };
   return (
     <div className="">
       product list
-      {productslist.map((products) => {
+      {Product.map((products) => {
         return <Products name={products.name} price={products.price} />;
       })}
+      <button onClick={productHandler}>Set Products</button>
     </div>
   );
 };
